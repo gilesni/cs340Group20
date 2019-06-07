3ad9304c977ce37ee795094523b10cca04aa1f49
Nicholas Giles <gilesni@flip1.engr.oregonstate.edu>
2019-06-07 15:46:13 -0700

    Created the repo

---
+++ dbcon.js
@@ -0,0 +1,10 @@
+var mysql = require('mysql');
+var pool = mysql.createPool({
+  connectionLimit : 10,
+  host            : 'classesmysql.engr.oregonstate.edu',
+  user            : 'cs340_gilesni',
+  password        : '4796',
+  database        : 'cs340_gilesni'
+});
+
+module.exports.pool = pool;
